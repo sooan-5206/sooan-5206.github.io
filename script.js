69f1fa19b62681b2f9520077eb2ea41251862616
sooan-5206 <annguyen050206@gmail.com>
2026-02-18 10:28:22 +0700

    Update script.js

--- script.js
+++ script.js
@@ -14,4 +14,16 @@ toggle.addEventListener("click", () => {
   document.body.classList.toggle("dark");
 });
 
+const items = document.querySelectorAll(".fade-in");
+
+window.addEventListener("scroll", () => {
+  items.forEach(item => {
+    const rect = item.getBoundingClientRect();
+    if (rect.top < window.innerHeight - 100) {
+      item.classList.add("show");
+    }
+  });
+});
+
+
 
